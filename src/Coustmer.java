f50d51a09e01b469fb74572a3e68991c9eaf4a9b
Administrator <1186945009>
2021-01-11 15:15:00 +0800

    1、飞机订票系统

---
+++ src/Coustmer.java
@@ -0,0 +1,40 @@
+public class Coustmer {
+    private String id;
+    private String name;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardid() {
+        return cardid;
+    }
+
+    public void setCardid(String cardid) {
+        this.cardid = cardid;
+    }
+
+    private String cardid;
+
+}
